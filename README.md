d0640cf0b3163e46c779450b9270c203d821b514
hudingding <18725515490@139.com>
2020-03-24 14:36:20 +0800

    更新README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # TridentNet-mmdetection
 TridentNet in mmdetection
+
+支持Backbobe为ResNet和ResNeXt中所有检测网络
+config参数修改，如下：
+
+backbone=dict(
+    type='TridentResNext', #TridentResNext 表示在ResNeXt基础上修改的， TridentResNet 表示在 ResNet基础上修改的
+    depth=101,
+    groups=64,
+    base_width=4,
+    num_stages=4,
+    out_indices=(0, 1, 2, 3),
+    frozen_stages=-1,
+    style='pytorch',
+    test_branch_idx=1,  # 推理时使用（0，1，2），0表示感受野最小的分支，2表示感受野最大的分支，1表示中间感受野的分支
+    dcn=dict(
+        modulated=False,
+        groups=64,
+        deformable_groups=1,
+        fallback_on_stride=False),
+    stage_with_dcn=(False, True, True, True)),
+
+测试结果
+在不增加运算量和参数量的前提下，相比于Bockbone为ResNet和ResNeXt网络，有一定的性能提升，具体视训练数据
--- cfg.py
+++ cfg.py
@@ -19,39 +19,4 @@ model = dict(
             deformable_groups=1,
             fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        num_outs=5),
-    rpn_head=dict(
-        type='RPNHead',
-        in_channels=256,
-        feat_channels=256,
-        anchor_scales=[4],
-        anchor_ratios=[0.5, 1.0, 2.0],
-        anchor_strides=[4, 8, 16, 32, 64],
-        target_means=[.0, .0, .0, .0],
-        target_stds=[1.0, 1.0, 1.0, 1.0],
-        loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
-    bbox_roi_extractor=dict(
-        type='SingleRoIExtractor',
-        roi_layer=dict(type='RoIAlign', out_size=7, sample_num=2),
-        out_channels=256,
-        featmap_strides=[4, 8, 16, 32]),
-    bbox_head=dict(
-        type='SharedFCBBoxHead',
-        num_fcs=2,
-        in_channels=256,
-        fc_out_channels=1024,
-        roi_feat_size=7,
-        num_classes=len(CLASSES)+1,
-        target_means=[0., 0., 0., 0.],
-        target_stds=[0.1, 0.1, 0.2, 0.2],
-        reg_class_agnostic=False,
-        loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)))
 #......
